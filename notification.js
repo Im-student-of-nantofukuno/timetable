bae2c2a6ecf0b4da16ae60cdd3251094b7166dad
Im-student-of-nantofukuno <3143250079@nantofukuno-h.tym.ed.jp>
2026-08-07 16:52:37 +0900

    Update notification.js

--- notification.js
+++ notification.js
@@ -3,19 +3,21 @@ window.NOTIFICATIONS = [
     id: "notice-ss-20260627",
     kind: "notice",
     title: "SS科より連絡",
-    range: "06/27〜06/28",
+    display_start: "2026-06-27T00:00:00+09:00",
+    display_end: "2026-06-28T23:59:59+09:00",
     body: "MM月DD日の古典に、国語辞典を持ってきてください。電子辞書でも構いません。",
     targets: {
-      grades: ["2"],
-      classes: ["4"],
-      courses: ["humanities"]
+        grades: ["2"],
+        classes: ["4"],
+        courses: ["humanities"]
     }
   },
   {
     id: "notice-ss-20260806",
     kind: "notice",
     title: "制作者より連絡",
-    range: "06/27〜09/28",
+    display_start: "2026-06-27T00:00:00+09:00",
+    display_end: "2026-09-28T23:59:59+09:00",
     body: "cloduflare連携チェック",
     targets: {
       grades: ["all"],
@@ -27,7 +29,8 @@ window.NOTIFICATIONS = [
     id: "notice-ss-20260620",
     kind: "notice",
     title: "SS科より連絡",
-    range: "06/20〜06/29",
+    display_start: "2026-06-20T00:00:00+09:00",
+    display_end:"2026-06-29T23:59:59+09:00",
     body: "6/29にワークを回収します。",
     targets: {
       grades: ["2"],
@@ -39,7 +42,8 @@ window.NOTIFICATIONS = [
     id: "notice-aa-20260625",
     kind: "notice",
     title: "AA委員会より連絡",
-    range: "06/25〜07/01",
+    display_start: "2026-06-25T00:00:00+09:00",
+    display_end:"2026-07-01T23:59:5909:00",
     body: "7/1にマナーアップを行います。頭髪や身だしなみをきちんと整える習慣を身につけましょう。",
     targets: {
       grades: ["all"],
@@ -51,7 +55,8 @@ window.NOTIFICATIONS = [
     id: "history-sample-1",
     kind: "history",
     title: "変更履歴",
-    range: "07/05",
+    display_start: "2026-07-05T00:00:00+09:00",
+    display_end: "2026-07-05T23:59:59+09:00",
     body: "2年4組文系の5限を古典に変更しました。",
     targets: {
       grades: ["2"],
@@ -63,7 +68,8 @@ window.NOTIFICATIONS = [
     id: "notice-aa-20260a25",
     kind: "notice",
     title: "AA委員会より連絡",
-    range: "06/25〜07/01",
+    display_start: "2026-06-25T00:00:00+09:00",
+    display_end:"2026-07-01T23:59:59+09:00",
     body: "7/1にマナーアップを行います。頭髪や身だしなみをきちんと整える習慣を身につけましょう。",
     targets: {
       grades: ["all"],
